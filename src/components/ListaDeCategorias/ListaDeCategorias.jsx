f7084d7640f060e61df59b111d1ceef43bc2252b
FranciscoStanley <franciscothestanley@gmail.com>
2021-12-17 00:54:13 -0300

    commit

--- src/components/ListaDeCategorias/ListaDeCategorias.jsx
+++ src/components/ListaDeCategorias/ListaDeCategorias.jsx
@@ -1,17 +1,30 @@
 import React, { Component } from "react";
+import "./ListaDeCategorias.css";
 
 class ListaDeCategorias extends Component {
-  state = {};
+  
+    _handlerEventInput(e) {
+    console.log(e.key);
+    if (e.key === "Enter") {
+      console.log("adicionar categoria");
+    }
+  }
+
   render() {
     return (
-      <div>
-        <ul>
-          <li>Categorias</li>
-          <li>Categorias</li>
-          <li>Categorias</li>
-          <li>Categorias</li>
+      <div className="lista-categorias">
+        <ul className="lista-categorias_lista">
+          <li className="lista-categorias_item">Categorias</li>
+          <li className="lista-categorias_item">Categorias</li>
+          <li className="lista-categorias_item">Categorias</li>
+          <li className="lista-categorias_item">Categorias</li>
         </ul>
-        <input type="text" />
+        <input
+          type="text"
+          className="lista-categorias_input"
+          placeholder="Adicionar Categoria"
+          onKeyUp={this._handlerEventInput.bind(this)}
+        />
       </div>
     );
   }
