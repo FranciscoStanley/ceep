eff1d6bea774ebfc2456c175ceeec602311ffd2d
FranciscoStanley <franciscothestanley@gmail.com>
2021-12-17 00:00:28 -0300

    Commit

--- src/App.js
+++ src/App.js
@@ -1,42 +1,45 @@
 import React, { Component } from "react";
 import ListaDeNotas from "./components/ListaDeNotas/ListaDeNotas";
 import Formulario from "./components/Fomulario/Formulario";
+import ListaDeCategorias from "./components/ListaDeCategorias";
 import "./assets/App.css";
 import "./assets/index.css";
 
 class App extends Component {
-  
   constructor() {
     super();
 
     this.state = {
-      notas:[]
-    }
+      notas: [],
+    };
   }
 
   criarNota(titulo, texto) {
-    const novaNota = {titulo, texto};
-    const novoArrayNotas = [...this.state.notas,novaNota]
+    const novaNota = { titulo, texto };
+    const novoArrayNotas = [...this.state.notas, novaNota];
     const novoEstado = {
-      notas:novoArrayNotas
-    }
+      notas: novoArrayNotas,
+    };
     this.setState(novoEstado);
   }
 
   deletarNota(index) {
     let arrayNotas = this.state.notas;
-    arrayNotas.splice(index,1);
-    this.setState({notas:arrayNotas})
+    arrayNotas.splice(index, 1);
+    this.setState({ notas: arrayNotas });
   }
 
   render() {
     return (
       <div className="conteudo">
         <Formulario criarNota={this.criarNota.bind(this)} />
-        <ListaDeNotas
-          apagarNota={this.deletarNota.bind(this)}
-          notas= {this.state.notas}
-        />
+        <main className="conteudo-principal">
+          <ListaDeCategorias />
+          <ListaDeNotas
+            apagarNota={this.deletarNota.bind(this)}
+            notas={this.state.notas}
+          />
+        </main>
       </div>
     );
   }
---
+++ src/components/ListaDeCategorias/ListaDeCategorias.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+class ListaDeCategorias extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <ul>
+          <li>Categorias</li>
+          <li>Categorias</li>
+          <li>Categorias</li>
+          <li>Categorias</li>
+        </ul>
+        <input type="text" />
+      </div>
+    );
+  }
+}
+
+export default ListaDeCategorias;
